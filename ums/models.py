b2c74af656e2c37ad0c93ba8d634523e0bb4b57d
puneetd <puneetd@dosepack.com>
2021-07-09 21:39:55 +0530

    Desc: api added for course and subjects

--- ums/models.py
+++ ums/models.py
@@ -3,16 +3,43 @@ from django.db import models
 # Create your models here.
 
 class StudentModel(models.Model):
-    reg_no = models.IntegerField()
-    first_name = models.CharField(max_length=30)
+    reg_no = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30, null=False)
     last_name = models.CharField(max_length=30)
-    father_name = models.CharField(max_length=30)
-    mother_name = models.CharField(max_length=30)
-    dob = dob = models.DateField(max_length=8)
-    contact_number = models.CharField(max_length=30)
-    email = models.EmailField(max_length = 254)
-    address = models.CharField(max_length=50)
-    city = models.CharField(max_length=30)
-    state = models.CharField(max_length=30)
-    country = models.CharField(max_length=30)
+    father_name = models.CharField(max_length=30, null=False)
+    mother_name = models.CharField(max_length=30, null=False)
+    # dob = dob = models.DateField(max_length=8, null=False)
+    contact_number = models.CharField(max_length=30, null=False, unique=True)
+    email = models.EmailField(max_length = 254, null=False, unique=True)
+    address = models.CharField(max_length=50, null=False)
+    city = models.CharField(max_length=30, null=False)
+    state = models.CharField(max_length=30, null=False)
+    country = models.CharField(max_length=30, null=False)
     
+class Courses(models.Model):
+    course_id = models.IntegerField(primary_key=True)
+    course_name = models.CharField(max_length=100, null=False, unique=True)
+    total_semester = models.IntegerField(null=False)
+
+class Streams(models.Model):
+    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    stream_id = models.IntegerField(primary_key=True)
+    stream_name = models.CharField(max_length=50, null=False)
+
+class Subjects(models.Model):
+    stream_id = models.ForeignKey(Streams, on_delete=models.CASCADE)
+    subject_name = models.CharField(max_length=50, null=False, unique=True)
+    semester = models.IntegerField(null=False)
+
+
+
+def update_auto_increment(start, table_name):
+    """Update our increments"""
+    from django.db import connection, transaction
+    cursor = connection.cursor()
+    q = "ALTER table {} AUTO_INCREMENT={}".format(table_name, start)
+    c = cursor.execute(q)
+    print("Update auto increment", c)
+    transaction.commit()
+
+# update_auto_increment(20210001, "ums_studentmodel")
--- ums/views.py
+++ ums/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from ums.models import *
+import json
 
 # Create your views here.
+
+def error(err):
+    return HttpResponse(json.dumps({"status": "error", "desc": str(err)}, indent=4))
+
+def success(data=None):
+    if not data:
+        data = True
+    return HttpResponse(json.dumps({"status": "success", "data": data}, indent=4))
+
+def get_student_data(request):
+    first_name = request.GET.get('first_name')
+    father_name = request.GET.get('father_name')
+    mother_name = request.GET.get('mother_name')
+    contact_number = request.GET.get('contact')
+    email = request.GET.get('email')
+    address = request.GET.get('address')
+    city = request.GET.get('city')
+    state = request.GET.get('state')
+    country = request.GET.get('country')
+    try:
+        StudentModel(first_name=first_name, father_name=father_name, mother_name=mother_name,
+                    contact_number=contact_number,email=email,address=address,city=city,state=state, country=country).save()
+        return success()
+    except Exception as e:
+        return error(e)
+
+def add_course(request):
+    course_id = request.GET.get('course_id')
+    course_name = request.GET.get('course_name')
+    total_semester = request.GET.get('total_semester')
+    try:
+        Courses(course_id, course_name, total_semester).save()
+        return success()
+    except Exception as e:
+        return error(e)
+
+def add_stream(request):
+    course_id = request.GET.get('course_id')
+    stream_id = request.GET.get('stream_id')
+    stream_name = request.GET.get('stream_name')
+    try:
+        Streams(course_id_id=course_id, stream_id=stream_id, stream_name=stream_name).save()
+        return success("Stream added sucessfully.")
+    except Exception as e:
+        return error(e)
+
+def add_subject(request):
+    subject_name = request.GET.get('subject_name')
+    semester = request.GET.get('semester')
+    stream_id = request.GET.get('stream_id')
+    try:
+        Subjects(stream_id_id=stream_id, subject_name=subject_name, semester=semester).save()
+        return success()
+    except Exception as e:
+        return error(e)
+
+def get_all_course(request):
+    """Join on multiple tables using filter"""
+    temp = {}
+    data = Subjects.objects.filter(stream_id__course_id = 123)
+    for i in data:
+        temp[i.stream_id.course_id.course_name] = 1
+    return success(temp)
